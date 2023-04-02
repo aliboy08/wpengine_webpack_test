e03d8408007423ebfd374ec956ddebb374723171
Alistair Ponce <alistair.ponce@gmail.com>
2023-04-02 19:59:15 +0900

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import FF_Filter from "./filter/filter";
 import FF_Ajax from "./filter/ajax";
 
-console.log('main.js!')
+console.log('main.js')
 var ff_filter = new FF_Filter({/* options here */});
 ff_filter.filter();
 
